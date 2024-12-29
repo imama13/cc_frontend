6fa52fcb8d49248a9d0e91a25cf5d0524d86d42b
ArsalanKhan04 <arsal4an@gmail.com>
2024-12-29 22:38:58 +0500

    Stats Section can now update usage live

--- src/components/StatsSection.js
+++ src/components/StatsSection.js
@@ -4,10 +4,11 @@ import { useAuth } from "../context/AuthContext";
 import "rc-slider/assets/index.css";
 import { useState } from "react";
 
-function StatsSection({ GetStore_Func, addsize, maxBandwidth = 100, maxStorage = 50 }) {
+function StatsSection({ GetStore_Func, sizestate, maxBandwidth = 100, maxStorage = 50 }) {
   const { user } = useAuth();
   const [storageUsed, setStorageUsed] = useState(0);
   const [bandwidthUsed, setBandwidthUsed] = useState(0);
+  const [addSize, setAddSize] = sizestate;
 
   const handleStorage = async () => {
     const bdy = await GetStore_Func(user.username);
@@ -15,6 +16,7 @@ function StatsSection({ GetStore_Func, addsize, maxBandwidth = 100, maxStorage =
   };
 
   handleStorage();
+  setAddSize(0);
 
   const bandwidthPercentage = Math.min((bandwidthUsed / maxBandwidth) * 100, 100);
   const storagePercentage = Math.min((storageUsed / maxStorage) * 100, 100);
